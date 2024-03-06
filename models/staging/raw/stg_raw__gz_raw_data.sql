608adb81b62162f366816601ea2944b3b288d16a
zxzxsha@gmail.com <zxzxsha@gmail.com>
2024-03-06 11:42:17 +0000

    r

--- models/staging/raw/stg_raw__gz_raw_data.sql
+++ models/staging/raw/stg_raw__gz_raw_data.sql
@@ -1,22 +1,17 @@
-with 
-
-source as (
-
-    select * from {{ source('raw', 'gz_raw_data') }}
-
+with source as (
+    select * from {{ source('raw_gs_sales', 'gz_raw_data') }}
 ),
 
+-- Define a transformation query
 renamed as (
-
     select
- date_date
-, orders_id 
- ,pdt_id
- ,revenue
- ,quantity
- 
+        date_date,
+        orders_id,
+        pdt_id,
+        revenue,
+        quantity
     from source
-
 )
 
-select * from renamed
+-- Final select from the transformed data
+select * from renamed;
