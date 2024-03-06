f87e3ea0d1adef2e734501cb46c86773b7b456eb
zxzxsha@gmail.com <zxzxsha@gmail.com>
2024-03-06 18:03:39 +0000

    ا

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,24 +1,10 @@
-WITH joined_data AS (
-    SELECT
-        s.date_date,
-        s.orders_id,
-        s.pdt_id,
-        s.quantity,
-        CAST(p.purchase_prise AS FLOAT64) AS purchase_price,
-         ROUND(s.quantity*CAST(p.purchase_prise AS FLOAT64),2) AS purchase_cost,
-        s.revenue
-    FROM {{ ref('stg_raw__sales') }} s
-    INNER JOIN {{ ref('stg_raw__product') }} p
-    ON s.pdt_id = p.products_id
-)
-
 SELECT
-    date_date,
-    orders_id,
-    pdt_id,
-    quantity,
-    purchase_price,
-    revenue,
-    purchase_cost,
-    ROUND(((revenue - (quantity * purchase_price)) / NULLIF(revenue, 0)), 3) AS margin
-FROM joined_data
+  orders_id,
+  max(date_date) as date_date,
+  ROUND(SUM(revenue),2) as revenue,
+  ROUND(SUM(quantity),2) as quantity,
+  ROUND(SUM(purchase_cost),2) as purchase_cost,
+  ROUND(SUM(margin),2) as margin
+FROM {{ ref("int_sales_margin") }}
+GROUP BY orders_id
+ORDER BY orders_id DESC
