b158d90cd97117929d560a5159789cead549178f
zxzxsha@gmail.com <zxzxsha@gmail.com>
2024-03-12 18:37:34 +0000

    m

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -4,7 +4,7 @@ SELECT
     orders_id,
     revenue, 
     quantity, 
-    CAST(purchase_prise AS FLOAT64), 
+    CAST(purchase_prise AS FLOAT64) as purchase_price ,
     ROUND(s.quantity*CAST(p.purchase_prise AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_prise AS FLOAT64),2) AS margin
 FROM {{ ref('stg_raw__sales') }} s
